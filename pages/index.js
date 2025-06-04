39268bd74e724da105c68e5851394f1874de4fba
massimogalioto <107213119+massimogalioto@users.noreply.github.com>
2025-06-04 15:54:22 +0200

    Delete pages directory

--- pages/index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main className="p-6">
-      <h1 className="text-2xl font-bold">Benvenuto</h1>
-      <p>Usa le pagine /upload-bolletta, /upload-bolletta-interna e /upload-cte.</p>
-    </main>
-  );
-}
